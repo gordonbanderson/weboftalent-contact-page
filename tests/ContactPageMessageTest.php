7d20a6c531ea344b0dac925df15205a83c4d2709
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-21 01:56:49 +0700

    ENHANCEMENT: Added a single test to check CMS fields

--- tests/ContactPageMessageTest.php
+++ tests/ContactPageMessageTest.php
@@ -4,6 +4,17 @@ class ContactPageMessageTest extends SapphireTest
 {
     public function testGetCMSFields()
     {
-        $this->markTestSkipped('TODO');
+        $cpm = new ContactPageMessage();
+        $fields = $cpm->getCMSFields();
+        $tab = $fields->findOrMakeTab('Root.Main');
+        $fields = $tab->FieldList();
+        $names = array();
+        foreach ($fields as $field) {
+            array_push($names, $field->getName());
+        }
+        $expected = array(
+            'Name', 'Email', 'Comments', 'RepliedTo'
+        );
+        $this->assertEquals($expected, $names);
     }
 }
